c214f20fa17667e6a009c108c114c931f571e069
Carlo Emilio MONTANARI <carlo.emilio.montanari@cern.ch>
2025-07-15 16:54:36 +0200

    Refactor logging functions to use structured logging format for improved readability

--- xboinc/server/tools.py
+++ xboinc/server/tools.py
@@ -19,39 +19,43 @@ def timestamp(ms=False, in_filename=True):
 
 def log_debug(message, cmd=None, is_server=False):
     if is_server:
-        cmd = '' if cmd is None else f'  {cmd}:'
-        lg.debug(f"DEBUG {timestamp(ms=True, in_filename=False)}"
-               + f"{cmd}  {message}")
+        cmd = "" if cmd is None else f"  {cmd}:"
+        lg.debug("DEBUG %s%s  %s", timestamp(ms=True, in_filename=False), cmd, message)
     else:
         print(message)
+
 
 def log_info(message, cmd=None, is_server=False):
     if is_server:
-        cmd = '' if cmd is None else f'  {cmd}:'
-        lg.info(f"INFO  {timestamp(ms=True, in_filename=False)}"
-               + f"{cmd}  {message}")
+        cmd = "" if cmd is None else f"  {cmd}:"
+        lg.info("INFO  %s%s  %s", timestamp(ms=True, in_filename=False), cmd, message)
     else:
         print(message)
 
+
 def log_error(message, e=None, cmd=None, is_server=False):
     if is_server:
-        cmd = '' if cmd is None else f'  {cmd}:'
-        lg.error(f"ERROR {timestamp(ms=True, in_filename=False)}"
-               + f"{cmd}  {message}", exc_info=True)
+        cmd = "" if cmd is None else f"  {cmd}:"
+        lg.error(
+            "ERROR %s%s  %s",
+            timestamp(ms=True, in_filename=False),
+            cmd,
+            message,
+            exc_info=True,
+        )
     else:
         raise Exception(message, e)
 
 
 def untar(filename, cmd=None, is_server=False):
-    cmd = 'untar' if cmd is None else cmd
+    cmd = "untar" if cmd is None else cmd
     try:
         filename = Path(filename)
         thisdir = filename.parent / filename.stem[:-4]
         thisdir.mkdir(parents=True, exist_ok=True)
-        with tarfile.open(filename, 'r:gz') as fid:
+        with tarfile.open(filename, "r:gz") as fid:
             fid.extractall(path=thisdir)
         filename.unlink()
         log_debug(f"Extracted {filename}.", cmd=cmd, is_server=is_server)
     except Exception as e:
         log_error(f"Failed extracting {filename}", e, cmd=cmd, is_server=is_server)
-
